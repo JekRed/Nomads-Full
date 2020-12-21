a14a35cacc38dfd5900e7964397dbae4e8372a39
Irfan Darmawan <74939210+JekRed@users.noreply.github.com>
2020-12-21 18:11:24 +0700

    update Laravel 8

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\TravelPackageController;
+use App\Http\Controllers\Admin\GalleryController;
+use App\Http\Controllers\Admin\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,44 +21,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index')
+Route::get('/', [HomeController::class,'index'])
     ->name('home');
 
-Route::get('/detail/{slug}', 'DetailController@index')
+Route::get('/detail/{slug}', [DetailController::class, 'index'])
     ->name('detail');
 
-Route::post('/checkout/{id}', 'CheckoutController@process')
+Route::post('/checkout/{id}', [CheckoutController::class, 'process'])
     ->name('checkout_process')
     ->middleware(['auth', 'verified']);
 
-Route::get('/checkout/{id}', 'CheckoutController@index')
+Route::get('/checkout/{id}', [CheckoutController::class, 'index'])
     ->name('checkout')
     ->middleware(['auth', 'verified']);
 
-Route::post('/checkout/create/{detail_id}', 'CheckoutController@create')
+Route::post('/checkout/create/{detail_id}', [CheckoutController::class, 'create'])
     ->name('checkout-create')
     ->middleware(['auth', 'verified']);
 
-Route::get('/checkout/remove/{detail_id}', 'CheckoutController@remove')
+Route::get('/checkout/remove/{detail_id}', [CheckoutController::class, 'remove'])
     ->name('checkout-remove')
     ->middleware(['auth', 'verified']);
 
-Route::get('/checkout/confirm/{id}', 'CheckoutController@success')
+Route::get('/checkout/confirm/{id}', [CheckoutController::class, 'success'])
     ->name('checkout-success')
     ->middleware(['auth', 'verified']);
 
 
 
 Route::prefix('admin')
-    ->namespace('Admin')
     ->middleware(['auth', 'admin'])
     ->group(function () {
-        Route::get('/', 'DashboardController@index')
+        Route::get('/', [DashboardController::class,'index'])
             ->name('dashboard');
 
 
-        Route::resource('travel-package', 'TravelPackageController');
-        Route::resource('gallery', 'GalleryController');
-        Route::resource('transaction', 'TransactionController');
+        Route::resource('travel-package', TravelPackageController::class);
+        Route::resource('gallery', GalleryController::class);
+        Route::resource('transaction', TransactionController::class);
     });
 Auth::routes(['verify' => true]);
